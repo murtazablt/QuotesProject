cdcd925a490d130bdcd6d8335936f1e2d9ee306a
Murtaza Bulut <bulut.2097@gmail.com>
2022-05-07 16:37:55 +0300

    sorting functionality activated

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,15 +1,16 @@
-import { Fragment } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Fragment } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const sortQuotes = (quotes, ascending) => {
+  
   return quotes.sort((quoteA, quoteB) => {
     if (ascending) {
-      return quoteA.id > quoteB.id ? 1 : -1;
+      return quotes.indexOf(quoteA) > quotes.indexOf(quoteB) ? 1 : -1;
     } else {
-      return quoteA.id < quoteB.id ? 1 : -1;
+      return quotes.indexOf(quoteA) < quotes.indexOf(quoteB) ? 1 : -1;
     }
   });
 };
@@ -20,13 +21,13 @@ const QuoteList = (props) => {
 
   const queryParams = new URLSearchParams(location.search);
 
-  const isSortingAscending = queryParams.get('sort') === 'asc';
+  const isSortingAscending = queryParams.get("sort") === "asc";
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
     navigate(location.pathname, {
-      search: `?sort=${isSortingAscending ? 'desc' : 'asc'}`,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`,
     });
   };
 
@@ -34,7 +35,7 @@ const QuoteList = (props) => {
     <Fragment>
       <div className={classes.sorting}>
         <button onClick={changeSortingHandler}>
-          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
         </button>
       </div>
       <ul className={classes.list}>
